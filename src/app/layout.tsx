8d9efbc2206e18886479db705b0dd77553f7ed2e
Aachal-10 <aagarwal@kairosliving.com>
2026-04-13 17:06:18 +0530

    Auth login and register changes

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,6 +2,8 @@ import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
 import { TRPCReactProvider } from "@/trpc/client";
+import { TooltipProvider } from "@/components/ui/tooltip";
+
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
@@ -29,7 +31,9 @@ export default function RootLayout({
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
       >
         <TRPCReactProvider>
+          <TooltipProvider>
         {children}
+        </TooltipProvider>
         </TRPCReactProvider>
       </body>
     </html>
